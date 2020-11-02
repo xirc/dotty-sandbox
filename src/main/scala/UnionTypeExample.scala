99ec7e4de0868a016067661df61c8292e4044846
Taichi Yamakawa <xirckey@gmail.com>
2020-11-02 23:00:45 +0900

    Add Union Type example

---
+++ src/main/scala/UnionTypeExample.scala
@@ -0,0 +1,18 @@
+/**
+ * See
+ *  - https://dotty.epfl.ch/docs/reference/new-types/union-types.html
+ */
+object UnionTypeExample extends App {
+  final case class Some[T](value: T)
+  final case object None
+  
+  type Option[T] = Some[T] | None.type
+  
+  def Option[T](value: T): Option[T] = {
+    if (value == null) None
+    else Some(value)
+  }
+  
+  println(Option("J"))
+  println(Option(null))
+}
