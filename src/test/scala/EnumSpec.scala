41902982997c9b491ab5d8403805d0f144d14da8
Taichi Yamakawa <xirckey@gmail.com>
2021-06-06 17:50:09 +0900

    Example: Enums
    https://dotty.epfl.ch/docs/reference/enums/enums.html

---
+++ src/test/scala/EnumSpec.scala
@@ -0,0 +1,67 @@
+object EnumSpec {
+
+  enum Size:
+    case Small, Medium, Large
+
+  enum Color(val rgba: Int):
+    case Red extends Color(0xff0000ff)
+    case Green extends Color(0x00ff00ff)
+    case Blue extends Color(0x0000ffff)
+    case Alpha extends Color(0x00000000)
+    def r: Int = (rgba >> 24) & 0x000000ff
+    def g: Int = (rgba >> 16) & 0x000000ff
+    def b: Int = (rgba >> 8) & 0x000000ff
+    def a: Int = rgba & 0x000000ff
+
+}
+
+/** [[https://dotty.epfl.ch/docs/reference/enums/enums.html Enumerations]]
+  */
+final class EnumSpec extends BaseSpec {
+  import EnumSpec._
+
+  "basic" in {
+
+    val size: Size = Size.Small
+    assert(size == Size.Small)
+
+    val sizeString: String = size match
+      case Size.Small  => "small"
+      case Size.Medium => "medium"
+      case Size.Large  => "large"
+
+    assert(sizeString == "small")
+
+    assert(Size.Small.ordinal == 0)
+    assert(Size.Medium.ordinal == 1)
+    assert(Size.Large.ordinal == 2)
+
+    assert(Size.valueOf("Small") == Size.Small)
+    assert(Size.valueOf("Medium") == Size.Medium)
+    assert(Size.valueOf("Large") == Size.Large)
+    assertThrows[IllegalArgumentException] {
+      Size.valueOf("Unknown")
+    }
+
+    assert(Size.values.toSeq == Seq(Size.Small, Size.Medium, Size.Large))
+
+    assert(Size.fromOrdinal(0) == Size.Small)
+    assert(Size.fromOrdinal(1) == Size.Medium)
+    assert(Size.fromOrdinal(2) == Size.Large)
+    assertThrows[NoSuchElementException] {
+      Size.fromOrdinal(3)
+    }
+
+  }
+
+  "paremeterized & members" in {
+
+    val red = Color.Red
+    assert(red.r == 255)
+    assert(red.g == 0)
+    assert(red.b == 0)
+    assert(red.a == 255)
+
+  }
+
+}
