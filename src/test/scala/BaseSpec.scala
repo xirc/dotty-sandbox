8b49737c2ae78f95ccbf417714b07bc37cd27d09
Taichi Yamakawa <xirckey@gmail.com>
2021-06-07 18:11:55 +0900

    Remove Matchers from the BaseSpec class

--- src/test/scala/BaseSpec.scala
+++ src/test/scala/BaseSpec.scala
@@ -1,4 +1,4 @@
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatest.matchers.should.Matchers
 
-abstract class BaseSpec extends AnyWordSpecLike with Matchers
+abstract class BaseSpec extends AnyWordSpecLike
