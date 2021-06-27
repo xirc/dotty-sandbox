f8b9b2f72dca4a77d874f3f3b89e5fdaa3c9f1f0
Taichi Yamakawa <xirckey@gmail.com>
2021-06-27 22:02:44 +0900

    Example: Transparent Traits
    https://dotty.epfl.ch/docs/reference/other-new-features/transparent-traits.html

---
+++ src/test/scala/TransparentTraitsSpecHelper.scala
@@ -0,0 +1,13 @@
+import scala.quoted._
+
+object TransparentTraitsSpecHelper {
+
+  inline def showElementType[T](xs: IterableOnce[T]): String =
+    showType[T]
+
+  inline def showType[T]: String =
+    ${ showTypeImpl(using Type.of[T]) }
+
+  def showTypeImpl[T](using Type[T])(using Quotes): Expr[String] = Expr(Type.show[T])
+
+}
