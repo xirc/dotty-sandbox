4258bbbb9864f3d44b6b49f83ce9996f5df5730f
Taichi Yamakawa <xirckey@gmail.com>
2021-07-01 20:55:40 +0900

    Example: Parameter Untupling
    https://dotty.epfl.ch/docs/reference/other-new-features/kind-polymorphism.html

---
+++ src/test/scala/ParameterUntuplingSpec.scala
@@ -0,0 +1,17 @@
+/** [[https://dotty.epfl.ch/docs/reference/other-new-features/parameter-untupling.html Parameter Untupling]]
+  */
+final class ParameterUntuplingSpec extends BaseSpec {
+
+  "untupling" in {
+
+    val xs = Vector("a", "b", "c", "d").zipWithIndex
+
+    val ys = xs.map { (x, y) => x * y }
+    assert(ys == Vector("", "b", "cc", "ddd"))
+
+    val zs = xs.map(_ * _)
+    assert(zs == Vector("", "b", "cc", "ddd"))
+
+  }
+
+}
