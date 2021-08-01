a495cb1341d14f9f5f2de7ee2ba6bce03ded407d
Taichi Yamakawa <xirckey@gmail.com>
2021-08-01 18:51:58 +0900

    Vararg Splices
    https://docs.scala-lang.org/scala3/reference/changed-features/vararg-splices.html

---
+++ src/test/scala/VarArgsSplicesSpec.scala
@@ -0,0 +1,18 @@
+/** [[https://docs.scala-lang.org/scala3/reference/changed-features/vararg-splices.html Vararg Splices]]
+  */
+final class VarArgsSplicesSpec extends BaseSpec {
+
+  "vararg splices" in {
+
+    val xs = Array(0, 1, 2, 3)
+    val ys = List(xs*)
+    val zs = ys match
+      case List(0, 1, ws*) => ws
+      case _               => ys
+
+    assert(ys == List(0, 1, 2, 3))
+    assert(zs == List(2, 3))
+
+  }
+
+}
